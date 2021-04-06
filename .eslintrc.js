1fe5267305afcc2862282cc562c97577371892eb
Vicente Reyes <vureyes@uc.cl>
2021-04-06 11:58:27 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-native',
+  ],
+  rules: {
+    'no-use-before-define': 0,
+  },
+};
